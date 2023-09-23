a5a09eb8f5f1c921c8ba13b216ab800e5e478b68
William Guilherme <wguilherme@securitygeek.io>
2023-09-22 21:20:03 -0700

    fix: separate zpa policy update rule function

--- zscaler/zpa/policies.py
+++ zscaler/zpa/policies.py
@@ -397,7 +397,86 @@ class PolicySetsAPI(APIEndpoint):
 
         return self._post(f"policySet/{policy_id}/rule", json=payload)
 
-    def update_rule(
+    def update_rule(self, policy_type: str, rule_id: str, **kwargs) -> Box:
+        """
+        Update an existing policy rule.
+
+        Ensure you are using the correct arguments for the policy type that you want to update.
+
+        Args:
+            policy_type (str):
+                The policy type. Accepted values are:
+
+                 |  ``access``
+                 |  ``timeout``
+                 |  ``client_forwarding``
+            rule_id (str):
+                The unique identifier for the rule to be updated.
+            **kwargs:
+                Optional keyword args.
+
+        Keyword Args:
+            action (str):
+                The action for the policy. Accepted values are:
+
+                |  ``allow``
+                |  ``deny``
+                |  ``intercept``
+                |  ``intercept_accessible``
+                |  ``bypass``
+            conditions (list):
+                A list of conditional rule tuples. Tuples must follow the convention: `Object Type`, `LHS value`,
+                `RHS value`. If you are adding multiple values for the same object type then you will need
+                a new entry for each value.
+                E.g.
+
+                .. code-block:: python
+
+                    [('app', 'id', '926196382959075416'),
+                    ('app', 'id', '926196382959075417'),
+                    ('app_group', 'id', '926196382959075332),
+                    ('client_type', 'zpn_client_type_exporter', 'zpn_client_type_zapp'),
+                    ('trusted_network', 'b15e4cad-fa6e-8182-9fc3-8125ee6a65e1', True)]
+            custom_msg (str):
+                A custom message.
+            description (str):
+                A description for the rule.
+            re_auth_idle_timeout (int):
+                The re-authentication idle timeout value in seconds.
+            re_auth_timeout (int):
+                The re-authentication timeout value in seconds.
+
+        Returns:
+            :obj:`Box`: The updated policy-rule resource record.
+
+        Examples:
+            Updates the name only for an Access Policy rule:
+
+            >>> zpa.policies.update_rule('access', '99999', name='new_rule_name')
+
+            Updates the action only for a Client Forwarding Policy rule:
+
+            >>> zpa.policies.update_rule('client_forwarding', '888888', action='BYPASS')
+
+        """
+        # Get policy id for specified policy type
+        policy_id = self.get_policy(policy_type).id
+
+        payload = convert_keys(self.get_rule(policy_type, rule_id))
+
+        # Add optional parameters to payload
+        for key, value in kwargs.items():
+            if key == "conditions":
+                payload["conditions"] = self._create_conditions(value)
+            else:
+                payload[snake_to_camel(key)] = value
+
+        resp = self._put(f"policySet/{policy_id}/rule/{rule_id}", json=payload, box=False).status_code
+
+        if resp == 204:
+            return self.get_rule(policy_type, rule_id)
+
+    def update_access_rule(
         self, policy_type: str, rule_id: str, app_connector_group_ids: list, app_server_group_ids: list, **kwargs
     ) -> Box:
         """
