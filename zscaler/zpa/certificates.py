d30406b09f0de6bc3fdde7234d2805dd3d258a8e
William Guilherme <wguilherme@securitygeek.io>
2023-10-04 08:51:34 -0700

    feat: Added post/delete method for zpa certificates

--- zscaler/zpa/certificates.py
+++ zscaler/zpa/certificates.py
@@ -18,7 +18,7 @@
 from box import Box, BoxList
 from restfly.endpoint import APIEndpoint, APISession
 
-from zscaler.utils import Iterator
+from zscaler.utils import Iterator, snake_to_camel
 
 
 class CertificatesAPI(APIEndpoint):
@@ -27,7 +27,7 @@ class CertificatesAPI(APIEndpoint):
 
         self.v2_url = api.v2_url
 
-    def list_browser_access(self, **kwargs) -> BoxList:
+    def list_issued_certificates(self, **kwargs) -> BoxList:
         """
         Returns a list of all Browser Access certificates.
 
@@ -54,7 +54,70 @@ class CertificatesAPI(APIEndpoint):
         """
         return BoxList(Iterator(self._api, f"{self.v2_url}/clientlessCertificate/issued", **kwargs))
 
-    def get_browser_access(self, certificate_id: str) -> Box:
+    def list_all_certificates(self, **kwargs) -> BoxList:
+        """
+        Returns a list of all Browser Access certificates.
+
+        Args:
+            **kwargs: Optional keyword args.
+
+        Keyword Args:
+            **max_items (int, optional):
+                The maximum number of items to request before stopping iteration.
+            **max_pages (int, optional):
+                The maximum number of pages to request before stopping iteration.
+            **pagesize (int, optional):
+                Specifies the page size. The default size is 20, but the maximum size is 500.
+            **search (str, optional):
+                The search string used to match against features and fields.
+
+        Returns:
+            :obj:`BoxList`: List of all Browser Access certificates.
+
+        Examples:
+            >>> for cert in zpa.certificates.list_all_certificates():
+            ...    print(cert)
+
+        """
+        return BoxList(Iterator(self._api, f"{self.v2_url}/certificate", **kwargs))
+
+    def add_certificate(self, name: str, cert_blob: str, **kwargs) -> Box:
+        """
+        Add a new Certificate.
+
+        Args:
+            name (str):
+                The name of the certificate.
+            cert_blob (str):
+                The content of the certificate.The certificateBlob field must be in string format and must include the certificate and the private key (in PEM format) in the JSON payload
+            **kwargs:
+                Optional keyword args.
+
+        Keyword Args:
+            description (str):
+                The description of the certificate.
+
+        Returns:
+            :obj:`Box`: The resource record for the newly created server.
+
+        Examples:
+            Create a certificate with the minimum required parameters:
+
+            >>> zpa.servers.add_server(
+            ...   name='myserver.example',
+            ...   cert_blob="-----BEGIN CERTIFICATE-----\nMIIFNzCCBIHNIHIO==\n-----END CERTIFICATE-----",'
+            )
+
+        """
+        payload = {"name": name, "certBlob": cert_blob}
+
+        # Add optional parameters to payload
+        for key, value in kwargs.items():
+            payload[snake_to_camel(key)] = value
+
+        return self._post("certificate", json=payload)
+
+    def get_certificate(self, certificate_id: str) -> Box:
         """
         Returns information on a specified Browser Access certificate.
 
@@ -71,6 +134,24 @@ class CertificatesAPI(APIEndpoint):
 
         """
         return self._get(f"clientlessCertificate/{certificate_id}")
+
+    def delete_certificate(self, certificate_id: str) -> Box:
+        """
+        Returns information on a specified Browser Access certificate.
+
+        Args:
+            certificate_id (str):
+                The unique identifier for the Browser Access certificate.
+
+        Returns:
+            :obj:`Box`:
+                The Browser Access certificate resource record.
+
+        Examples:
+            >>> ba_certificate = zpa.certificates.get_certificate('99999')
+
+        """
+        return self._get(f"certificate/{certificate_id}")
 
     def get_enrolment(self, certificate_id: str) -> Box:
         """
