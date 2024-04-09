73107053a2af45acd8804c75bd004a46d7b663b7
William Guilherme <wguilherme@securitygeek.io>
2024-04-09 14:26:38 -0700

    fix: Implemented detailed retry error code logic

--- tests/integration/zia/test_users.py
+++ tests/integration/zia/test_users.py
@@ -15,7 +15,7 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import pytest
-
+import time
 from tests.integration.zia.conftest import MockZIAClient
 from tests.test_utils import generate_random_password, generate_random_string
 
@@ -70,13 +70,16 @@ class TestUsers:
         except Exception as exc:
             errors.append(f"User creation or activation failed: {exc}")
             
+        time.sleep(1)  # Add a sleep before fetching the user
+        
         try:
             # Fetch and verify the user
             retrieved_user = client.users.get_user(user_id)
             assert retrieved_user["id"] == user_id, "Incorrect user account retrieved"
         except Exception as exc:
             errors.append(f"Retrieving User Account failed: {exc}")
-            
+        time.sleep(1)  # Add a sleep before listing users
+        
         try:
                 # Update the User Account
                 updated_password = generate_random_password()  # Generate a new password
@@ -89,12 +92,52 @@ class TestUsers:
         except Exception as exc:
             errors.append(f"Updating User Account password failed or activation after update failed: {exc}")
 
+        try:
+            # Example 1: List users using default settings
+            all_users = client.users.list_users()
+            assert all_users, "Failed to list users with default settings."
+            time.sleep(1)  # Add a sleep before listing users
+            
+            # Example 2: List users, limiting to a maximum of 10 items
+            limited_users = client.users.list_users(max_items=10)
+            assert len(limited_users) <= 10, "Failed to limit the list of users to max_items=10."
+            time.sleep(1)  # Add a sleep before listing users
+            
+            # Example 3: List users, returning 200 items per page for a maximum of 2 pages
+            paginated_users = client.users.list_users(page_size=200, max_pages=2)
+            # Since we can't directly assert the total pages without fetching all data, we focus on page size
+            assert len(paginated_users) <= 400, "Failed to paginate users with page_size=200 and max_pages=2."
+            time.sleep(1)  # Add a sleep before listing users
+            
+            # Additional example: Filter by department and group with a `starts with` match
+            filtered_users_dept = client.users.list_users(dept="A000")
+            assert filtered_users_dept, "Failed to filter users by department name."
+            time.sleep(1)  # Add a sleep before listing users
+            
+            filtered_users_group = client.users.list_users(group="A000")
+            assert filtered_users_group, "Failed to filter users by group name."
+            time.sleep(1)  # Add a sleep before listing users
+            
+            # Additional example: Filter by user name with a `partial` match
+            filtered_users_name = client.users.list_users(name="tests")
+            assert filtered_users_name, "Failed to filter users by partial name match."
+            time.sleep(1)  # Add a sleep before finally deleting the user
+            
+            # Check if the newly created user is in one of the lists of users
+            found_user_default = any(user["id"] == user_id for user in all_users)
+            found_user_limited = any(user["id"] == user_id for user in limited_users)
+            found_user_paginated = any(user["id"] == user_id for user in paginated_users)
+            assert found_user_default or found_user_limited or found_user_paginated, "Newly created user account not found in any list of users."
+        except Exception as exc:
+            errors.append(f"Listing users with specific parameters failed: {exc}")
+
         finally:
             if user_id:
                 try:
                     # Delete the User Account
                     delete_status = client.users.delete_user(user_id)
                     assert delete_status == 204, "User Account deletion failed"
+                    time.sleep(1)  # Add a sleep before activation
                     
                     # Reactivate Configuration after Deletion
                     activation_response = client.activate.activate()
@@ -145,7 +188,7 @@ class TestUsers:
 
         try:
             # List groups with optional parameters
-            groups = client.users.list_groups(page_size=2, max_pages=1)
+            groups = client.users.list_groups(page_size=2, max_pages=1, sort_order="ASC")
             assert len(groups) <= 2, "More groups returned than expected with page_size=2"
             assert isinstance(groups, list), "Expected a list of groups"
             if groups:
