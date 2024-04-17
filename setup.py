8af11268bd3bc58ec5f52470316c907b57ad1c16
William Guilherme <wguilherme@securitygeek.io>
2024-04-17 09:15:25 -0700

    doc: Fixed README badges

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
