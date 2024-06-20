e20f0f6abeb6c034ebb6ded046ed7b06dfb7987c
William Guilherme <wguilherme@zscaler.com>
2024-06-19 21:30:38 -0700

    feat: Improved ZIA/ZPA Gateway and Forwarding Control Rule (#111)
    * feat: Improved ZIA/ZPA Gateway and Forwarding Control Rule
    * fix: Upgraded poetry packages

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "0.1.2"
+version = "0.41.0"
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.41.0"
 
 # -- General configuration ---------------------------------------------------
 
