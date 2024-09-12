c0c8227becec346c35e0c2f96b6cf5a59fdf6c4b
William Guilherme <wguilherme@securitygeek.io>
2024-09-12 09:56:30 -0700

    fix: Fixed ZIA DLP Engine description missing attribute in payload construction

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "0.9.1"
+version = "0.9.2"
 # The full version, including alpha/beta/rc tags
-release = "0.9.1"
+release = "0.9.2"
 
 # -- General configuration ---------------------------------------------------
 
