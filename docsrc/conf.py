86faf86b6823f4f1169d59fb2f5f327348d68cbe
William Guilherme <wguilherme@zscaler.com>
2025-04-22 16:02:28 -0700

    fix: release stable v1.0.2 version (#260)

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "1.0.1"
+version = "1.0.2"
 # The full version, including alpha/beta/rc tags
-release = "1.0.1"
+release = "1.0.2"
 
 # -- General configuration ---------------------------------------------------
 
