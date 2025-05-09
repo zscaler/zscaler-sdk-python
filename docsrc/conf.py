6ace0f8b722b0b3f07930d5fad886feb06e69901
William Guilherme <wguilherme@zscaler.com>
2025-05-09 00:36:03 -0700

    fix: Fixed Documentation formatting issue (#277)

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "1.2.0"
+version = "1.2.4"
 # The full version, including alpha/beta/rc tags
-release = "1.2.0"
+release = "1.2.4"
 
 # -- General configuration ---------------------------------------------------
 
