ea3e1040173ab7d22fe92fb0c87fb900f2af8080
William Guilherme <wguilherme@zscaler.com>
2025-05-27 11:06:05 -0700

    fix: Fixed ZPA and ZIA Models and utility functions (#292)
    * fix: Fixed ZPA and ZIA Models and utility functions

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "1.4.0"
+version = "1.4.1"
 # The full version, including alpha/beta/rc tags
-release = "1.4.0"
+release = "1.4.1"
 
 # -- General configuration ---------------------------------------------------
 
