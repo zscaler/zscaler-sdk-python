674043a138af6d1f8c3e8539573715ed44c05f20
William Guilherme <wguilherme@zscaler.com>
2025-08-29 00:50:19 -0700

    fix: Fixed Non Standard attribute in Location Management Model (#361)

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "1.7.7"
+version = "1.7.8"
 # The full version, including alpha/beta/rc tags
-release = "1.7.7"
+release = "1.7.8"
 
 # -- General configuration ---------------------------------------------------
 
