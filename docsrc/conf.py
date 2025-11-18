2a0836d22b9bacad8a0d3ab97a06b06e82afe611
William Guilherme <wguilherme@securitygeek.io>
2025-11-18 09:24:24 -0800

    fix: Fixed Type Hint in ZIA Cloud Firewalls and other resources

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "1.9.1"
+version = "1.9.2"
 # The full version, including alpha/beta/rc tags
-release = "1.9.1"
+release = "1.9.2"
 
 # -- General configuration ---------------------------------------------------
 
