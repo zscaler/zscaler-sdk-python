407106a1807147b41d835f58b3f411ae3b5574f5
William Guilherme <wguilherme@securitygeek.io>
2025-12-11 09:37:15 -0800

    fix: Fixed ZPA Legacy Rate limiting check

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,9 +28,9 @@ author = "Zscaler Technology Alliances"
 html_title = ""
 
 # The short X.Y version
-version = "1.9.8"
+version = "1.9.9"
 # The full version, including alpha/beta/rc tags
-release = "1.9.8"
+release = "1.9.9"
 
 # -- General configuration ---------------------------------------------------
 
--- zscaler/__init__.py
+++ zscaler/__init__.py
@@ -29,7 +29,7 @@ __license__ = "MIT"
 __contributors__ = [
     "William Guilherme",
 ]
-__version__ = "1.9.8"
+__version__ = "1.9.9"
 
 
 from zscaler.oneapi_client import Client as ZscalerClient  # noqa
